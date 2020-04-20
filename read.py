01d65786d943fa5c7093c943919747dfba477876
Vany Liu <vany8462@gmail.com>
2020-04-20 14:56:33 +0800

    Count average characters in each comment

--- read.py
+++ read.py
@@ -6,7 +6,11 @@ with open('reviews.txt','r') as f:
 		count += 1
 		if count % 10000 == 0:
 			print(len(data))
+print('Done reading file. Total: ', len(data), 'data')
 
-print(data[0])
-print('---------------')
-print(data[1])
+# calculate the average characters in each comment.
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+average = sum_len / len(data)
+print('Average characters in each comment: ', average)
